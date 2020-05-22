7c34a7c7cff544665c6429ed005d6533444563d9
Drew Peters <peters.drew.dp@gmail.com>
2020-05-22 09:25:03 -0400

    Updated ers client urls

--- src/remote/ers-client.ts
+++ src/remote/ers-client.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const ersClient = axios.create({
-    // baseURL: 'http://p1api-env.eba-6w57avct.us-east-1.elasticbeanstalk.com/',
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://p1api-env.eba-6w57avct.us-east-1.elasticbeanstalk.com/',
+    // baseURL: 'http://localhost:8080/',
+    
     headers: {
         'Content-Type': 'application/json'
     },
